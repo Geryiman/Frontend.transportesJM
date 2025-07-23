292a697c3b497f5c563645db88dff381e7bdf84d
Geryiman <geryiman0@gmail.com>
2025-07-23 12:25:58 -0600

    Update CrearUsuario.jsx

--- src/pages/admin/CrearUsuario.jsx
+++ src/pages/admin/CrearUsuario.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
-import '../../styles/crearusuario.css';
+import '../../styles/CrearUsuario.css';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
